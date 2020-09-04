ddd838d8139f37ec57e39005d32fc5c4e11711cd
Promise Fru <promisefru1@gmail.com>
2020-09-04 16:39:34 +0100

    date_string empty return current timestamp

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (rq, res) => {
-    res.send(console.log("Hello express"))
+app.get("/api/timestamp/", (rq, res) => {
+    res.json({"unix": Date.now(), "utc": Date()});
 });
 
 app.listen(port, console.log(`Listening on port ${port}`))
